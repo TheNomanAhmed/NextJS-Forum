2ec31b9185e318901c63e9250ed243a906044718
Noman Ahmed Khan <nomanahmed478@gmail.com>
2020-10-07 03:34:11 +0300

    wip: initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+
+  purge: ["./src/**/*.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
